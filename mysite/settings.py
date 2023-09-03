982b349487e132e4fb221620df7c31015d0830f8
Aleksandr <mendelev.basic@gmail.com>
2023-09-03 13:56:05 +0200

    add gunicorn and nginx

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-oywmle&tqg((g9%c)qib#t5tnw4n9jtpsc%!psgt8fr^1fy4hu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv("DEBUG", "1")))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
